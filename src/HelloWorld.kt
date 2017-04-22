ff31bdffea72f5151c1d7dd293f884a7b1e33ab8
ManiekSas2012 <adidas98@poczta.fm>
2017-04-22 18:37:02 +0200

    Array list

--- src/HelloWorld.kt
+++ src/HelloWorld.kt
@@ -2,12 +2,9 @@
  * Created by Sasquatch on 2017-04-22.
  */
 fun main(args: Array<String>) {
-    val mode: Int = 2
 
-    when(mode) {
-        1 -> println("Mode 1")
-        2 -> {
-            println("Mode 2")
-        }
-    }
+    val arrayList = arrayListOf("Adam")
+    arrayList.add("Ela")
+
+    arrayList.forEach(::println)
 }
